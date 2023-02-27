9418ad9185c68483a26002ec90574ed465820635
hossain101199 <fshossain10@gmail.com>
2023-02-27 16:48:36 +0600

    Metaversus first commit with Navbar and Hero section

---
+++ sections/About.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02" />
+    <motion.div>hello</motion.div>
+  </section>
+);
+
+export default About;
